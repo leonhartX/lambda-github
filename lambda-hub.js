25f3db5743f7e4bec1ef27ad5699168f51ae1de0
Ke Xu <leonhartx.k@gmail.com>
2016-09-17 09:18:34 +0900

    add diff support

--- lambda-hub.js
+++ lambda-hub.js
@@ -6,11 +6,19 @@ chrome.runtime.onMessage.addListener(() => {
   if($('.github').length === 0) {
     initContext()
     .then(initLambdaList)
-    .then(getGithubRepos)
     .then(initPageContent)
+    .then(getGithubRepos)
+    .then(updateRepo)
+    .then(updateBranch)
     .catch((err) => {
-      if (err.message === "need login") {
-        initLoginContent();
+      switch (err.message) {
+        case "need login" :
+          initLoginContent();
+          break;
+        case "nothing" :
+          break;
+        default:
+          console.log(err);
       }
     });
   }
@@ -32,6 +40,10 @@ $(() => {
   $(document).on('click', '.github-repo-model-dismiss', (event) => {
     changeModelState('repo', false);
   })
+  $(document).on('click', '#github-create-repo', (event) => {
+    changeModelState('repo', false);
+    githubCreateRepo();
+  });
 
   $(document).on('click', '#github-new-branch', showCreateBranch);
   $(document).on('input propertychange', '#new-branch-name', (event) => {
@@ -40,9 +52,21 @@ $(() => {
   $(document).on('click', '.github-branch-model-dismiss', (event) => {
     changeModelState('branch', false);
   })
+  $(document).on('click', '#github-create-branch', (event) => {
+    changeModelState('branch', false);
+    githubCreateBranch();
+  });
 
-  $(document).on('click', '#github-pull', gitPull);
-  $(document).on('click', '#github-push', gitPush);
+  $(document).on('click', '.github-diff-model-dismiss', (event) => {
+    changeModelState('diff', false);
+  })
+
+  $(document).on('click', '.github-alert-dismiss', (event) => {
+    $(event.target).parents('.github-alert').remove();
+  });
+
+  $(document).on('click', '#github-pull', githubPull);
+  $(document).on('click', '#github-push', githubPush);
   $(document).on('click', '#github-login', (event) => {
     if (chrome.runtime.openOptionsPage) {
       // New way to open options pages, if supported (Chrome 42+).
@@ -103,66 +127,80 @@ $(() => {
   });
 });
 
-
-function gitPull() {
-  const path = context.current.runtime.indexOf("nodejs") >= 0 ? "index.js" : "index.py";
-  $.getJSON(
-    `${baseUrl}/repos/${context.repo.fullName}/contents/${path}?ref=${context.branch}`,
-    { access_token: accessToken }
-  )
-  .then((data) => {
-    return $.get(data.download_url);
-  })
-  .then((code) => {
-    const payload = {
-      operation: "updateFunctionCode",
-      codeSource: "inline",
-      functionName: context.functionName,
-      handler: context.current.handler,
-      runtime: context.current.runtime,
-      inline: code
-    };
-    return $.ajax({
-      url: 'https://' + context.endpoint + '/lambda/services/ajax?operation=updateFunctionCode',
+function checkDiff() {
+  return Promise.all([
+    $.getJSON(
+      `${baseUrl}/repos/${context.repo.fullName}/contents/${context.file}?ref=${context.branch}`,
+      { access_token: accessToken }
+    )
+    .then((data) => {
+      return $.get(data.download_url);
+    }),
+    $.ajax({
+      url: 'https://' + context.endpoint + '/lambda/services/ajax?operation=getFunctionCode',
       headers: {
         "X-Csrf-Token" : context.csrf
       },
       method: 'POST',
       crossDomain: true,
+      dataType: 'json',
       contentType: 'application/json',
-      data: JSON.stringify(payload)
-    });
-  })
-  .then(() => {
-    console.log("pull ok");
-    location.reload();
-  })
-  .fail((err) => {
-    console.log(err);
-    showAlert("Failed to pull");
-  });
+      data: JSON.stringify({
+        functionName: context.functionName,
+        qualifier: context.qualifier,
+        operation: "getFunctionCode"
+      })
+    })
+  ])
+  .then((data) => {
+    const diff = JsDiff.createTwoFilesPatch(context.file, context.file, data[0], data[1].code);
+    const diffHtml = new Diff2HtmlUI({diff : diff});
+    diffHtml.draw('.github-diff', {inputFormat: 'json', outputFormat:'side-by-side', showFiles: false});
+    diffHtml.highlightCode('.github-diff');
+    changeModelState('diff', true);
+    return {
+      github: data[0],
+      lambda: data[1].code
+    }
+  })  
 }
 
-function gitPush() {
-  const payload = {
-    functionName: context.functionName,
-    qualifier: context.qualifier,
-    operation: "getFunctionCode"
-  };
-  $.ajax({
-    url: 'https://' + context.endpoint + '/lambda/services/ajax?operation=getFunctionCode',
-    headers: {
-      "X-Csrf-Token" : context.csrf
-    },
-    method: 'POST',
-    crossDomain: true,
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(payload)
-  })
+function githubPull() {
+  checkDiff()
+  // .then((data) => {
+  //   const payload = {
+  //     operation: "updateFunctionCode",
+  //     codeSource: "inline",
+  //     functionName: context.functionName,
+  //     handler: context.current.handler,
+  //     runtime: context.current.runtime,
+  //     inline: data.github
+  //   };
+  //   return $.ajax({
+  //     url: 'https://' + context.endpoint + '/lambda/services/ajax?operation=updateFunctionCode',
+  //     headers: {
+  //       "X-Csrf-Token" : context.csrf
+  //     },
+  //     method: 'POST',
+  //     crossDomain: true,
+  //     contentType: 'application/json',
+  //     data: JSON.stringify(payload)
+  //   });
+  // })
+  // .then(() => {
+  //   console.log("pull ok");
+  //   location.reload();
+  // })
+  // .catch((err) => {
+  //   showAlert("Failed to pull", "error");
+  // });
+}
+
+function githubPush() {
+  checkDiff()
   .then((data) => {
     const payload = {
-      content: data.code,
+      content: data.lambda,
       encoding: "utf-8"
     };
     return $.ajax({
@@ -191,11 +229,10 @@ function gitPush() {
     });
   })
   .then((response) => {
-    const path = context.current.runtime.indexOf("nodejs") >= 0 ? "index.js" : "index.py";
     const payload = {
       base_tree: response.base_tree,
       tree : [{
-        path: path,
+        path: context.file,
         mode: "100644",
         type: "blob",
         sha: response.sha
@@ -254,11 +291,105 @@ function gitPush() {
     });
   })
   .then(() => {
+    showAlert(`Successfully push to ${context.branch} of ${context.repo.name}`);
     console.log("push ok");
   })
-  .fail((err) => {
+  .catch((err) => {
     console.log(err);
-    showAlert("Failed to push");
+    showAlert("Failed to push", "error");
+  });
+}
+
+function githubCreateRepo() {
+  const repo = $('#new-repo-name').val();
+  const desc = $('#new-repo-desc').val();
+  const payload = {
+    name : repo,
+    description : desc
+  }
+  if (!repo || repo === "") return;
+  $.ajax({
+    url: `${baseUrl}/user/repos`,
+    headers: {
+      "Authorization": `token ${accessToken}`
+    },
+    method: 'POST',
+    crossDomain: true,
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify(payload)
+  })
+  .then((response) => {
+    const repo = {
+      name : response.name,
+      fullName : response.full_name
+    };
+    context.repo = repo;
+    Object.assign(context.bindRepo, { [context.functionName] : repo });
+    chrome.storage.sync.set({ bindRepo: context.bindRepo });
+    return response;
+  })
+  .then(getGithubRepos)
+  .then(updateRepo)
+  .then(updateBranch)
+  .then(() => {
+    showAlert(`Successfully create new repository ${repo}`);
+  })
+  .fail((err) => {
+    showAlert("Failed to create new repository.", "error");
+  });
+}
+
+function githubCreateBranch() {
+  const branch = $('#new-branch-name').val();
+  if (!branch || branch === "") return;
+  $.getJSON(
+    `${baseUrl}/repos/${context.repo.fullName}/git/refs/heads/master`,
+    { access_token: accessToken }
+  )
+  .then((response) => {
+    if (response.object) {
+      return response.object.sha;
+    }
+    else {
+      return $.getJSON(
+        `${baseUrl}/repos/${context.repo.fullName}/git/refs/heads`,
+        { access_token: accessToken }
+      )
+      .then((response) => {
+        return response[0].object.sha;
+      })
+    }
+  })
+  .then((sha) => {
+    const payload = {
+      ref: `refs/heads/${branch}`,
+      sha: sha
+    };
+    return $.ajax({
+      url: `${baseUrl}/repos/${context.repo.fullName}/git/refs`,
+      headers: {
+        "Authorization": `token ${accessToken}`
+      },
+      method: 'POST',
+      crossDomain: true,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(payload)
+    });
+  })
+  .then((response) => {
+    context.branch = branch;
+    Object.assign(context.bindBranch, { [context.functionName] : branch });
+    chrome.storage.sync.set({ bindBranch: context.bindBranch });
+    return context.repo.name;
+  })
+  .then(updateBranch)
+  .then(() => {
+    showAlert(`Successfully create new branch: ${branch}`);
+  })
+  .fail((err) => {
+    showAlert("Failed to create new branch.", "error");
   });
 }
 
@@ -274,7 +405,7 @@ function showCreateBranch() {
 
 function initContext() {
   context = {};
-  const match = window.location.href.match(/https:\/\/(.*?)\/.*functions\/(\w*)(\?|\/)((.*)\?)?/);
+  const match = window.location.href.match(/https:\/\/(.*?)\/.*functions\/(.*?)(\?|\/)((.*)\?)?/);
   if (!match) return null;
   context.endpoint = match[1];
   context.functionName = match[2];
@@ -317,6 +448,7 @@ function initLambdaList() {
       context.functions[lambda.name] = lambda
       if (lambda.name === context.functionName) {
         context.current = lambda;
+        context.file = lambda.runtime.indexOf("nodejs") >= 0 ? "index.js" : "index.py";
       }
     })
   });
@@ -346,29 +478,20 @@ function getGithubRepos() {
   })
 }
 
-function initPageContent(repos) {
+function initPageContent() {
   const div = $('.awsmob-button-group');
   if($('.github').length !== 0 || div.length === 0 || div.children().length <= 2) {
-    return;
+    throw new Error("nothing to do");
   }
-
-  $.get(chrome.runtime.getURL('content/buttons.html'))
-  .then((content) => {
-    div.children().last().after(content);
-    $('#github-repos').append('<li><a id="github-new-repo">Create new repo</a></li>');
-    repos.forEach((repo) => {
-      let liContent = `<li><a class="github-repo" data=${repo.fullName}>${repo.name}</a></li>`
-      $('#github-repos').append(liContent);
-    });
-    if (context.repo) {
-      $('#github-bind-repo').text(`Repo: ${context.repo.name}`);
-      updateBranch(context.repo.name);
-    }
-  });
 
   $.get(chrome.runtime.getURL('content/model.html'))
   .then((content) => {
     $('#main').siblings().last().after(content);
+  });
+
+  return $.get(chrome.runtime.getURL('content/buttons.html'))
+  .then((content) => {
+    return div.children().last().after(content);
   });
 }
 
@@ -387,8 +510,24 @@ function initLoginContent() {
   div.children().last().after(htmlContent);
 }
 
-function updateBranch(repo) {
-  $.getJSON(
+function updateRepo(repos) {
+  $('#github-repos').empty().append('<li><a id="github-new-repo">Create new repo</a></li>');
+  repos.forEach((repo) => {
+    let liContent = `<li><a class="github-repo" data=${repo.fullName}>${repo.name}</a></li>`
+    $('#github-repos').append(liContent);
+  });
+  if (context.repo) {
+    $('#github-bind-repo').text(`Repo: ${context.repo.name}`);
+    return context.repo.name;
+  }
+  return null;
+}
+
+function updateBranch() {
+  if (!context.repo) {
+    return null;
+  }
+  return $.getJSON(
     `${baseUrl}/repos/${context.repo.fullName}/branches`,
     { access_token: accessToken }
   )
@@ -400,8 +539,10 @@ function updateBranch(repo) {
     });
     let branch = context.bindBranch[context.functionName];
     if (!branch) {
-      if (branches.length === 0) branch = "";
-      else if ($.inArray(branch, branches.map(branch => branch.name)) < 0) {
+      if (branches.length === 0) {
+        branch = "";
+        showAlert("This repository do not has any branch yet, try to create a new branch such as [master].", "warning");
+      } else if ($.inArray(branch, branches.map(branch => branch.name)) < 0) {
         branch = ($.inArray("master", branches.map(branch => branch.name)) >= 0) ? "master" : branches[0].name;
       }
     }
@@ -410,8 +551,8 @@ function updateBranch(repo) {
     context.branch = branch;
     Object.assign(context.bindBranch, { [context.functionName] : branch });
     chrome.storage.sync.set({ bindBranch: context.bindBranch });
+    return branch;
   })
-  .fail(console.log);
 }
 
 function changeModelState(type, toShow) {
@@ -436,8 +577,11 @@ function changeButtonState(type, value) {
   }
 }
 
-function showAlert(msg) {
-  setTimeout(() => {
-    alert(msg);
-  }, 0);
+//show alert using aws ui
+//level: info, warning, error
+function showAlert(message, level="info") {
+  $.get(chrome.runtime.getURL('content/alert.html'))
+  .then((content) => {
+    $('.content').before(content.replace(/_INFO_/g, level).replace(/_MESSAGE_/, message));
+  });
 }
